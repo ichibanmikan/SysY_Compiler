d6ed30b0826929149d9233fd69867c7e5f506f7d
ichibanmikan <ichibanmikans@gmail.com>
2022-05-18 20:38:51 +0800

    增加了对不同进制整数的支持

--- src/SysY_Lex/SysY_Lex.h
+++ src/SysY_Lex/SysY_Lex.h
@@ -4,12 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 // #include "../../include/includeLex.h"
 #include "../../include/createTree.h"
 #include "../SysY_Yacc/SysY_Yacc.tab.h"
 
+#define INT_MAX 2147483647
+
 // int numNode;
 // void SysY_lex(char* input_file, Token_Node* token_stream);
 int count_num_enter(char *string);
+int HextoDec(char *str);
+int OcttoDec(char* text);
+const char* InttoString(int num);
+void setNewChar(char* ch, char* p);
 
 #endif
