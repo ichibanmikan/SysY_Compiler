596d81aee4417ff0c6e577bb24be01a818ba44cb
ichibanmikan <ichibanmikans@gmail.com>
2022-05-25 19:10:26 +0800

    修改了test.py脚本

--- test/test.py
+++ test/test.py
@@ -8,7 +8,7 @@ if(('out' in currpath) or ('test' in currpath)):
 	os.chdir('../')
 outfile = './out/'
 syfiles = './test/'
-logfile = './test/log.txt'
+logfile = './test/error.log'
 error = '\033[1;31;40m%s\033[0m'
 info = '\033[1;32;40m%s\033[0m'
 warning = '\033[1;34;40m%s\033[0m'
