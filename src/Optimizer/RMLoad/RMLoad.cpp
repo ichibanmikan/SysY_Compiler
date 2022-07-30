569bb3647a3575f739b2dee36f3896ec971994ae
ichibanmikan <ichibanmikans@gmail.com>
2022-07-30 18:53:31 +0800

    取消优化函数

--- src/Optimizer/RMLoad/RMLoad.cpp
+++ src/Optimizer/RMLoad/RMLoad.cpp
@@ -1,13 +1,13 @@
-#include "RMLoad.h"
+// #include "RMLoad.h"
 
-void remove_load(){
-  for(auto iter=functions_table.begin(); iter!=functions_table.end(); iter++){
-    remove_load_func(iter->second);
-  }
-}
+// void remove_load(){
+//   for(auto iter=functions_table.begin(); iter!=functions_table.end(); iter++){
+//     remove_load_func(iter->second);
+//   }
+// }
 
-inline void remove_load_func(Function* func){
-  for(int i=0; i<func->basic_blocks->size(); i++){
+// inline void remove_load_func(Function* func){
+//   for(int i=0; i<func->basic_blocks->size(); i++){
 
-  }
-}
+//   }
+// }
--- src/Optimizer/SSA/SSA.cpp
+++ src/Optimizer/SSA/SSA.cpp
@@ -1,4 +1,4 @@
-void turn_to_SSA(){
-  for(auto iter=functions_table.begin(); iter!=functions_table.end(); iter++){
-  }
-}
+// void turn_to_SSA(){
+//   for(auto iter=functions_table.begin(); iter!=functions_table.end(); iter++){
+//   }
+// }
